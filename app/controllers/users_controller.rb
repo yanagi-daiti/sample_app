a5f88a85dfa3124c4a9e3553f27e6ee1858b8427
yanagi-daiti <daiti.yanagisawa@gmail.com>
2015-02-17 13:47:39 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,11 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
+def create
     @user = User.new(user_params)
     if @user.save
-      # 保存の成功をここで扱う。
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
