87f2fda5cedfc6d43ac56a23835bb7fc5cfa042a
yanagi-daiti <daiti.yanagisawa@gmail.com>
2015-02-18 14:22:18 +0900

    lesson_9.1.2_start

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -12,7 +12,8 @@ describe "Authentication" do
 
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
-    end    
+    end
+
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
@@ -24,7 +25,7 @@ describe "Authentication" do
       it { should have_title(user.name) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
-      it { should_not have_link('Sign in', href: signin_path) }
+      it { should_not have_link('Sign in', href: signin_path)    
 
       describe "followed by signout" do
         before { click_link "Sign out" }
